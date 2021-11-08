27f0452f0b1284acaf93d3de4926836109b905e0
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-11-08 21:27:59 +0100

    Create entity type configurations

---
+++ src/ChargingStations.Infrastructure/Configurations/ChargerModelEntityTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using ChargingStations.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChargingStations.Infrastructure.Configurations
+{
+    public class ChargerModelEntityTypeConfiguration : IEntityTypeConfiguration<ChargerModel>
+    {
+        public void Configure(EntityTypeBuilder<ChargerModel> builder)
+        {
+            builder.ToTable("charger_model");
+
+            builder.HasKey(x => x.ChargerModelId);
+
+            builder.Property(x => x.ChargerModelId)
+                .HasColumnName("id")
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .IsRequired()
+                .HasMaxLength(255);
+            
+            builder.Property(x => x.Manufacturer)
+                .HasColumnName("manufacturer")
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
