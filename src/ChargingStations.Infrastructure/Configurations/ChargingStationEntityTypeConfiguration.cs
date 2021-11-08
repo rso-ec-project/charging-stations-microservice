f5e1971955f271ec35464c46c62dba2031855153
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-11-08 21:44:42 +0100

    Fix namespace issue when entities were relocated 2

--- src/ChargingStations.Infrastructure/Configurations/ChargingStationEntityTypeConfiguration.cs
+++ src/ChargingStations.Infrastructure/Configurations/ChargingStationEntityTypeConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using ChargingStations.Domain.Entities;
+﻿using ChargingStations.Domain.ChargingStationAggregate;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
