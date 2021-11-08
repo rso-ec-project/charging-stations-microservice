812e60e6f2b869f69a95dbc1b93f3217e6c26e9d
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-11-08 21:30:36 +0100

    Create ApplicationDBContext file

---
+++ src/ChargingStations.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ChargingStations.Domain.Entities;
+using ChargingStations.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChargingStations.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<ChargingStation> ChargingStations { get; set; }
+        public DbSet<ChargerModel> ChargerModels { get; set; }
+        public DbSet<Charger> Chargers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.ApplyConfiguration(new TenantEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ChargerModelEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ChargingStationEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ChargerModelEntityTypeConfiguration());
+        }
+    }
+}
