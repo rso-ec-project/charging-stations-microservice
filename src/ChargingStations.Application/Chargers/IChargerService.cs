8f0d711b4e113df34f783f92a95f3a8457265652
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-11-14 16:15:37 +0100

    Create Chargers GetById endpoint

---
+++ src/ChargingStations.Application/Chargers/IChargerService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ChargingStations.Application.Chargers
+{
+    public interface IChargerService
+    {
+        Task<ChargerDto> GetAsync(int chargerId);
+    }
+}
