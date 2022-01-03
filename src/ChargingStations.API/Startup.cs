c55a7e7c070687c73aab35b795be628ec54fc286
Anzx <31044008+anzetomazin@users.noreply.github.com>
2022-01-03 22:10:27 +0100

    Update Startup.cs

--- src/ChargingStations.API/Startup.cs
+++ src/ChargingStations.API/Startup.cs
@@ -22,7 +22,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Polly;
 using Steeltoe.Common.Http.Discovery;
 using Steeltoe.Discovery.Client;
 using Steeltoe.Discovery.Consul;
