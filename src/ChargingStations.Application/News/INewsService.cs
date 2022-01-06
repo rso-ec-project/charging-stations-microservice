7a53e34eeff3dbee78b865f8925880fc4d2e1dea
Anzx <31044008+anzetomazin@users.noreply.github.com>
2022-01-06 18:13:58 +0100

    Add News endpoint that serves news from external API

---
+++ src/ChargingStations.Application/News/INewsService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChargingStations.Application.News
+{
+    public interface INewsService
+    {
+        Task<List<NewsDto>> GetAsync();
+    }
+}
---
+++ src/ChargingStations.Application/News/NewsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ChargingStations.Application.News
+{
+    public class NewsDto
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Source { get; set; }
+    }
+}
